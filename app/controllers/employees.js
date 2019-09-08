0e7b47ef1b3cf09bac52e7334352704c47008a47
bhflm <germanbonin@gmail.com>
2019-09-08 00:28:45 -0300

    (refactor): changed async for promises

--- app/controllers/employees.js
+++ app/controllers/employees.js
@@ -4,8 +4,12 @@ const employeesService = require('../services/employees');
 
 exports.getList = async (req, res) => {
   logger.info(`Employees list request with query params ${Util.inspect(req.query, { depth: null })}`);
-  const employeesRes = await employeesService.getList(req.query);
-  return res.json(employeesRes.data);
+  return employeesService.getList(req.query)
+    .then(response => res.json(response.data))
+    .catch(err => {
+      logger.error(err);
+      return err;
+    });
 };
 
 exports.getDetail = async (req, res) => {
@@ -13,6 +17,10 @@ exports.getDetail = async (req, res) => {
     ${Util.inspect({ params: req.params, query: req.query }, { depth: null })}`);
   //TODO: api/{resource}/:id is broken, {missing auth token}
   const id = `id=${req.params.id}`; //temporary fix:
-  const employeesRes = await employeesService.getDetail(id);
-  return res.json(employeesRes.data);
+  return employeesService.getDetail(id)
+    .then(response => res.json(response.data))
+    .catch(err => {
+      logger.error(err);
+      return err;
+    });
 };
