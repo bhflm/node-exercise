f176feb91a74ba946943559bcb8abc18f9c11724
bhflm <germanbonin@gmail.com>
2019-09-07 23:39:16 -0300

    (refactor): deleted mocks

--- tests/controllers/employees.test.js
+++ tests/controllers/employees.test.js
@@ -1,38 +1,33 @@
 const request = require('supertest'),
   nock = require('nock'),
   server = require('../../app'),
-  { API } = require('../../app/config'),
-  { employeesMock } = require('../mocks');
+  { API } = require('../../app/config');
 
 describe('Employees tests', () => {
   test('Get employees list with status = 200', done => {
-
-  nock(API.BigCorp)
-    .get('/employees')
-    .reply(200, { data: employeesMock });
-
     return request(server)
       .get('/employees')
       .end((err, res) => {
-        expect(res.body.data.length).not.toBe(0);
-        expect(res.body.data[0]).toHaveProperty('first');
-        expect(res.body.data[0]).toHaveProperty('last');
-        expect(res.body.data[0]).toHaveProperty('id');
-        expect(res.body.data[0]).toHaveProperty('manager');
-        expect(res.body.data[0]).toHaveProperty('department');
-        expect(res.body.data[0]).toHaveProperty('office');
+        expect(res.body.length).not.toBe(0);
+        expect(res.body[0]).toHaveProperty('first');
+        expect(res.body[0]).toHaveProperty('last');
+        expect(res.body[0]).toHaveProperty('id');
+        expect(res.body[0]).toHaveProperty('manager');
+        expect(res.body[0]).toHaveProperty('department');
+        expect(res.body[0]).toHaveProperty('office');
         expect(res.status).toBe(200);
         done();
       });
   });
 
-  // test('Get employees with query with limit 15', done => {
-  //
-  //   nock(API.BigCorp)
-  //     .get('/employees')
-  //     .query( {limit: 15})
-  //     .reply(200, { data: employeesMock });
-  //
-  // })
+  test('Get employees with query with limit 15', done => {
+      return request(server)
+        .get('/employees?limit=15')
+        .end((err, res) => {
+          expect(res.body.length).toBe(15);
+          expect(res.status).toBe(200);
+          done();
+        });
+  })
 
 });
