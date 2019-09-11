a1741a1ec6feaa358ecb6bed419762519de2d92f
bhflm <germanbonin@gmail.com>
2019-09-11 20:24:37 -0300

    (wip): mock get offices list controller

--- app/controllers/offices.js
+++ app/controllers/offices.js
@@ -1,13 +1,13 @@
 const Util = require('util');
-const departmentsService = require('../services/departments');
+const officesService = require('../services/offices');
 const logger = require('../logger');
 const { DEFAULT_LIMIT, DEFAULT_OFFSET } = require('../constants');
 
 exports.getList = (req, res) => {
-  logger.info(`Departments list request with query params ${Util.inspect(req.query, { depth: null })}`);
+  logger.info(`Offices list request with query params ${Util.inspect(req.query, { depth: null })}`);
   const { limit, offset } = req.query.limit && req.query.offset ? req.query : { limit : DEFAULT_LIMIT, offset: DEFAULT_OFFSET };
-  return departmentsService // todo: limit & offset
-    .getMultipleDepartments({limit, offset})
+  return officesService // todo: limit & offset
+    .getMultipleOffices({limit, offset})
     .then(response => res.json({ response: response.data} ))
     .catch(errResponse => {
       logger.error(errResponse.erorr);
