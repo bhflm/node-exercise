362f3e070fc18e547df78c1dd38e10f7bd74a7bb
bhflm <germanbonin@gmail.com>
2019-09-12 00:23:28 -0300

    (add): offset&limit for employees queries

--- app/controllers/departments.js
+++ app/controllers/departments.js
@@ -5,10 +5,11 @@ const { DEFAULT_LIMIT, DEFAULT_OFFSET } = require('../constants');
 
 exports.getList = (req, res) => {
   logger.info(`Departments list request with query params ${Util.inspect(req.query, { depth: null })}`);
-  const { limit, offset } = req.query.limit && req.query.offset ? req.query : { limit : DEFAULT_LIMIT, offset: DEFAULT_OFFSET };
+  const { limit, offset } =
+    req.query.limit && req.query.offset ? req.query : { limit: DEFAULT_LIMIT, offset: DEFAULT_OFFSET };
   return departmentsService // todo: limit & offset
-    .getMultipleDepartments({limit, offset})
-    .then(response => res.json({ response: response.data} ))
+    .getMultipleDepartments({ limit, offset })
+    .then(response => res.json({ response: response.data }))
     .catch(errResponse => {
       logger.error(errResponse.erorr);
       return res.status(400).send(errResponse.error);
--- app/controllers/offices.js
+++ app/controllers/offices.js
@@ -5,10 +5,11 @@ const { DEFAULT_LIMIT, DEFAULT_OFFSET } = require('../constants');
 
 exports.getList = (req, res) => {
   logger.info(`Offices list request with query params ${Util.inspect(req.query, { depth: null })}`);
-  const { limit, offset } = req.query.limit && req.query.offset ? req.query : { limit : DEFAULT_LIMIT, offset: DEFAULT_OFFSET };
+  const { limit, offset } =
+    req.query.limit && req.query.offset ? req.query : { limit: DEFAULT_LIMIT, offset: DEFAULT_OFFSET };
   return officesService // todo: limit & offset
-    .getMultipleOffices({limit, offset})
-    .then(response => res.json({ response: response.data} ))
+    .getMultipleOffices({ limit, offset })
+    .then(response => res.json({ response: response.data }))
     .catch(errResponse => {
       logger.error(errResponse.erorr);
       return res.status(400).send(errResponse.error);
