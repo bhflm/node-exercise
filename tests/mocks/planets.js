d512c1ca87e9905604296ccf8ec29316e092e610
German Bonin <germanbonin@gmail.com>
2020-06-13 20:37:45 -0300

    Add: swapi planets unit tests

---
+++ tests/mocks/planets.js
@@ -0,0 +1,428 @@
+exports.onePage = {
+    "count": 60,
+    "next": "http://swapi.dev/api/planets/?page=2",
+    "previous": null,
+    "results": [
+        {
+            "name": "Tatooine",
+            "rotation_period": "23",
+            "orbital_period": "304",
+            "diameter": "10465",
+            "climate": "arid",
+            "gravity": "1 standard",
+            "terrain": "desert",
+            "surface_water": "1",
+            "population": "200000",
+            "residents": [
+                "http://swapi.dev/api/people/1/",
+                "http://swapi.dev/api/people/2/",
+                "http://swapi.dev/api/people/4/",
+                "http://swapi.dev/api/people/6/",
+                "http://swapi.dev/api/people/7/",
+                "http://swapi.dev/api/people/8/",
+                "http://swapi.dev/api/people/9/",
+                "http://swapi.dev/api/people/11/",
+                "http://swapi.dev/api/people/43/",
+                "http://swapi.dev/api/people/62/"
+            ],
+            "films": [
+                "http://swapi.dev/api/films/1/",
+                "http://swapi.dev/api/films/3/",
+                "http://swapi.dev/api/films/4/",
+                "http://swapi.dev/api/films/5/",
+                "http://swapi.dev/api/films/6/"
+            ],
+            "created": "2014-12-09T13:50:49.641000Z",
+            "edited": "2014-12-20T20:58:18.411000Z",
+            "url": "http://swapi.dev/api/planets/1/"
+        },
+        {
+            "name": "Alderaan",
+            "rotation_period": "24",
+            "orbital_period": "364",
+            "diameter": "12500",
+            "climate": "temperate",
+            "gravity": "1 standard",
+            "terrain": "grasslands, mountains",
+            "surface_water": "40",
+            "population": "2000000000",
+            "residents": [
+                "http://swapi.dev/api/people/5/",
+                "http://swapi.dev/api/people/68/",
+                "http://swapi.dev/api/people/81/"
+            ],
+            "films": [
+                "http://swapi.dev/api/films/1/",
+                "http://swapi.dev/api/films/6/"
+            ],
+            "created": "2014-12-10T11:35:48.479000Z",
+            "edited": "2014-12-20T20:58:18.420000Z",
+            "url": "http://swapi.dev/api/planets/2/"
+        },
+        {
+            "name": "Yavin IV",
+            "rotation_period": "24",
+            "orbital_period": "4818",
+            "diameter": "10200",
+            "climate": "temperate, tropical",
+            "gravity": "1 standard",
+            "terrain": "jungle, rainforests",
+            "surface_water": "8",
+            "population": "1000",
+            "residents": [],
+            "films": [
+                "http://swapi.dev/api/films/1/"
+            ],
+            "created": "2014-12-10T11:37:19.144000Z",
+            "edited": "2014-12-20T20:58:18.421000Z",
+            "url": "http://swapi.dev/api/planets/3/"
+        },
+        {
+            "name": "Hoth",
+            "rotation_period": "23",
+            "orbital_period": "549",
+            "diameter": "7200",
+            "climate": "frozen",
+            "gravity": "1.1 standard",
+            "terrain": "tundra, ice caves, mountain ranges",
+            "surface_water": "100",
+            "population": "unknown",
+            "residents": [],
+            "films": [
+                "http://swapi.dev/api/films/2/"
+            ],
+            "created": "2014-12-10T11:39:13.934000Z",
+            "edited": "2014-12-20T20:58:18.423000Z",
+            "url": "http://swapi.dev/api/planets/4/"
+        },
+        {
+            "name": "Dagobah",
+            "rotation_period": "23",
+            "orbital_period": "341",
+            "diameter": "8900",
+            "climate": "murky",
+            "gravity": "N/A",
+            "terrain": "swamp, jungles",
+            "surface_water": "8",
+            "population": "unknown",
+            "residents": [],
+            "films": [
+                "http://swapi.dev/api/films/2/",
+                "http://swapi.dev/api/films/3/",
+                "http://swapi.dev/api/films/6/"
+            ],
+            "created": "2014-12-10T11:42:22.590000Z",
+            "edited": "2014-12-20T20:58:18.425000Z",
+            "url": "http://swapi.dev/api/planets/5/"
+        },
+        {
+            "name": "Bespin",
+            "rotation_period": "12",
+            "orbital_period": "5110",
+            "diameter": "118000",
+            "climate": "temperate",
+            "gravity": "1.5 (surface), 1 standard (Cloud City)",
+            "terrain": "gas giant",
+            "surface_water": "0",
+            "population": "6000000",
+            "residents": [
+                "http://swapi.dev/api/people/26/"
+            ],
+            "films": [
+                "http://swapi.dev/api/films/2/"
+            ],
+            "created": "2014-12-10T11:43:55.240000Z",
+            "edited": "2014-12-20T20:58:18.427000Z",
+            "url": "http://swapi.dev/api/planets/6/"
+        },
+        {
+            "name": "Endor",
+            "rotation_period": "18",
+            "orbital_period": "402",
+            "diameter": "4900",
+            "climate": "temperate",
+            "gravity": "0.85 standard",
+            "terrain": "forests, mountains, lakes",
+            "surface_water": "8",
+            "population": "30000000",
+            "residents": [
+                "http://swapi.dev/api/people/30/"
+            ],
+            "films": [
+                "http://swapi.dev/api/films/3/"
+            ],
+            "created": "2014-12-10T11:50:29.349000Z",
+            "edited": "2014-12-20T20:58:18.429000Z",
+            "url": "http://swapi.dev/api/planets/7/"
+        },
+        {
+            "name": "Naboo",
+            "rotation_period": "26",
+            "orbital_period": "312",
+            "diameter": "12120",
+            "climate": "temperate",
+            "gravity": "1 standard",
+            "terrain": "grassy hills, swamps, forests, mountains",
+            "surface_water": "12",
+            "population": "4500000000",
+            "residents": [
+                "http://swapi.dev/api/people/3/",
+                "http://swapi.dev/api/people/21/",
+                "http://swapi.dev/api/people/35/",
+                "http://swapi.dev/api/people/36/",
+                "http://swapi.dev/api/people/37/",
+                "http://swapi.dev/api/people/38/",
+                "http://swapi.dev/api/people/39/",
+                "http://swapi.dev/api/people/42/",
+                "http://swapi.dev/api/people/60/",
+                "http://swapi.dev/api/people/61/",
+                "http://swapi.dev/api/people/66/"
+            ],
+            "films": [
+                "http://swapi.dev/api/films/3/",
+                "http://swapi.dev/api/films/4/",
+                "http://swapi.dev/api/films/5/",
+                "http://swapi.dev/api/films/6/"
+            ],
+            "created": "2014-12-10T11:52:31.066000Z",
+            "edited": "2014-12-20T20:58:18.430000Z",
+            "url": "http://swapi.dev/api/planets/8/"
+        },
+        {
+            "name": "Coruscant",
+            "rotation_period": "24",
+            "orbital_period": "368",
+            "diameter": "12240",
+            "climate": "temperate",
+            "gravity": "1 standard",
+            "terrain": "cityscape, mountains",
+            "surface_water": "unknown",
+            "population": "1000000000000",
+            "residents": [
+                "http://swapi.dev/api/people/34/",
+                "http://swapi.dev/api/people/55/",
+                "http://swapi.dev/api/people/74/"
+            ],
+            "films": [
+                "http://swapi.dev/api/films/3/",
+                "http://swapi.dev/api/films/4/",
+                "http://swapi.dev/api/films/5/",
+                "http://swapi.dev/api/films/6/"
+            ],
+            "created": "2014-12-10T11:54:13.921000Z",
+            "edited": "2014-12-20T20:58:18.432000Z",
+            "url": "http://swapi.dev/api/planets/9/"
+        },
+        {
+            "name": "Kamino",
+            "rotation_period": "27",
+            "orbital_period": "463",
+            "diameter": "19720",
+            "climate": "temperate",
+            "gravity": "1 standard",
+            "terrain": "ocean",
+            "surface_water": "100",
+            "population": "1000000000",
+            "residents": [
+                "http://swapi.dev/api/people/22/",
+                "http://swapi.dev/api/people/72/",
+                "http://swapi.dev/api/people/73/"
+            ],
+            "films": [
+                "http://swapi.dev/api/films/5/"
+            ],
+            "created": "2014-12-10T12:45:06.577000Z",
+            "edited": "2014-12-20T20:58:18.434000Z",
+            "url": "http://swapi.dev/api/planets/10/"
+        }
+    ]
+};
+
+exports.lastPage = {
+    "count": 60,
+    "next": null,
+    "previous": "http://swapi.dev/api/planets/?page=5",
+    "results": [
+        {
+            "name": "Mirial",
+            "rotation_period": "unknown",
+            "orbital_period": "unknown",
+            "diameter": "unknown",
+            "climate": "unknown",
+            "gravity": "unknown",
+            "terrain": "deserts",
+            "surface_water": "unknown",
+            "population": "unknown",
+            "residents": [
+                "http://swapi.dev/api/people/64/",
+                "http://swapi.dev/api/people/65/"
+            ],
+            "films": [],
+            "created": "2014-12-20T16:44:46.318000Z",
+            "edited": "2014-12-20T20:58:18.508000Z",
+            "url": "http://swapi.dev/api/planets/51/"
+        },
+        {
+            "name": "Serenno",
+            "rotation_period": "unknown",
+            "orbital_period": "unknown",
+            "diameter": "unknown",
+            "climate": "unknown",
+            "gravity": "unknown",
+            "terrain": "rainforests, rivers, mountains",
+            "surface_water": "unknown",
+            "population": "unknown",
+            "residents": [
+                "http://swapi.dev/api/people/67/"
+            ],
+            "films": [],
+            "created": "2014-12-20T16:52:13.357000Z",
+            "edited": "2014-12-20T20:58:18.510000Z",
+            "url": "http://swapi.dev/api/planets/52/"
+        },
+        {
+            "name": "Concord Dawn",
+            "rotation_period": "unknown",
+            "orbital_period": "unknown",
+            "diameter": "unknown",
+            "climate": "unknown",
+            "gravity": "unknown",
+            "terrain": "jungles, forests, deserts",
+            "surface_water": "unknown",
+            "population": "unknown",
+            "residents": [
+                "http://swapi.dev/api/people/69/"
+            ],
+            "films": [],
+            "created": "2014-12-20T16:54:39.909000Z",
+            "edited": "2014-12-20T20:58:18.512000Z",
+            "url": "http://swapi.dev/api/planets/53/"
+        },
+        {
+            "name": "Zolan",
+            "rotation_period": "unknown",
+            "orbital_period": "unknown",
+            "diameter": "unknown",
+            "climate": "unknown",
+            "gravity": "unknown",
+            "terrain": "unknown",
+            "surface_water": "unknown",
+            "population": "unknown",
+            "residents": [
+                "http://swapi.dev/api/people/70/"
+            ],
+            "films": [],
+            "created": "2014-12-20T16:56:37.250000Z",
+            "edited": "2014-12-20T20:58:18.514000Z",
+            "url": "http://swapi.dev/api/planets/54/"
+        },
+        {
+            "name": "Ojom",
+            "rotation_period": "unknown",
+            "orbital_period": "unknown",
+            "diameter": "unknown",
+            "climate": "frigid",
+            "gravity": "unknown",
+            "terrain": "oceans, glaciers",
+            "surface_water": "100",
+            "population": "500000000",
+            "residents": [
+                "http://swapi.dev/api/people/71/"
+            ],
+            "films": [],
+            "created": "2014-12-20T17:27:41.286000Z",
+            "edited": "2014-12-20T20:58:18.516000Z",
+            "url": "http://swapi.dev/api/planets/55/"
+        },
+        {
+            "name": "Skako",
+            "rotation_period": "27",
+            "orbital_period": "384",
+            "diameter": "unknown",
+            "climate": "temperate",
+            "gravity": "1",
+            "terrain": "urban, vines",
+            "surface_water": "unknown",
+            "population": "500000000000",
+            "residents": [
+                "http://swapi.dev/api/people/76/"
+            ],
+            "films": [],
+            "created": "2014-12-20T17:50:47.864000Z",
+            "edited": "2014-12-20T20:58:18.517000Z",
+            "url": "http://swapi.dev/api/planets/56/"
+        },
+        {
+            "name": "Muunilinst",
+            "rotation_period": "28",
+            "orbital_period": "412",
+            "diameter": "13800",
+            "climate": "temperate",
+            "gravity": "1",
+            "terrain": "plains, forests, hills, mountains",
+            "surface_water": "25",
+            "population": "5000000000",
+            "residents": [
+                "http://swapi.dev/api/people/77/"
+            ],
+            "films": [],
+            "created": "2014-12-20T17:57:47.420000Z",
+            "edited": "2014-12-20T20:58:18.519000Z",
+            "url": "http://swapi.dev/api/planets/57/"
+        },
+        {
+            "name": "Shili",
+            "rotation_period": "unknown",
+            "orbital_period": "unknown",
+            "diameter": "unknown",
+            "climate": "temperate",
+            "gravity": "1",
+            "terrain": "cities, savannahs, seas, plains",
+            "surface_water": "unknown",
+            "population": "unknown",
+            "residents": [
+                "http://swapi.dev/api/people/78/"
+            ],
+            "films": [],
+            "created": "2014-12-20T18:43:14.049000Z",
+            "edited": "2014-12-20T20:58:18.521000Z",
+            "url": "http://swapi.dev/api/planets/58/"
+        },
+        {
+            "name": "Kalee",
+            "rotation_period": "23",
+            "orbital_period": "378",
+            "diameter": "13850",
+            "climate": "arid, temperate, tropical",
+            "gravity": "1",
+            "terrain": "rainforests, cliffs, canyons, seas",
+            "surface_water": "unknown",
+            "population": "4000000000",
+            "residents": [
+                "http://swapi.dev/api/people/79/"
+            ],
+            "films": [],
+            "created": "2014-12-20T19:43:51.278000Z",
+            "edited": "2014-12-20T20:58:18.523000Z",
+            "url": "http://swapi.dev/api/planets/59/"
+        },
+        {
+            "name": "Umbara",
+            "rotation_period": "unknown",
+            "orbital_period": "unknown",
+            "diameter": "unknown",
+            "climate": "unknown",
+            "gravity": "unknown",
+            "terrain": "unknown",
+            "surface_water": "unknown",
+            "population": "unknown",
+            "residents": [
+                "http://swapi.dev/api/people/82/"
+            ],
+            "films": [],
+            "created": "2014-12-20T20:18:36.256000Z",
+            "edited": "2014-12-20T20:58:18.525000Z",
+            "url": "http://swapi.dev/api/planets/60/"
+        }
+    ]
+};
--- tests/services/swapi.js
+++ tests/services/swapi.js
@@ -3,15 +3,16 @@ const nock = require('nock');
 const config = require('../../app/config');
 const swapiService = require('../../app/services/swapi');
 const peopleMock = require('../mocks/people');
+const planetsMock = require('../mocks/planets');
 
 const mockEndpoint = ({ route, query }, statusCode, responseObj) =>
   nock(`${config.API.SWAPI}`)
     .get(`/${route}${query || ''}`)
     .reply(statusCode, responseObj);
 
-describe('[SERVICE]: People tests', () => {
+describe('[SERVICE]: Swapi', () => {
   let params = { route: 'people', query: '' };
-  test('Get valid page returns people', () => {
+  test('Get valid people page returns people', () => {
     params = { route: 'people', query: '?page=1' };
     const endpoint = mockEndpoint(params, 200, peopleMock.onePage);
     return swapiService.get(params).then(obj => {
@@ -21,20 +22,55 @@ describe('[SERVICE]: People tests', () => {
       expect(obj.data.results.length).toBe(10);
     });
   });
-  test('Get invalid page returns 404', () => {
+  test('Get invalid people page returns 404', () => {
     params = { route: 'people', query: '?page=1000000' };
     const endpoint = mockEndpoint(params, 404, {});
     return swapiService.get(params).then(obj => {
       expect(obj.response.status).toBe(404);
     });
   });
-  test('Get last page returns people, but no next page ref', () => {
+  test('Get last people page returns people, but no next page ref', () => {
     params = { route: 'people', query: '?page=9' };
     const endpoint = mockEndpoint(params, 200, peopleMock.lastPage);
     return swapiService.get(params).then(obj => {
       expect(obj.data).toHaveProperty('count');
       expect(obj.data.count).toBe(82);
       expect(obj.data.next).toBe(null);
+    });
+  });
+  test('Get valid first planets page returns planets', () => {
+    params = { route: 'planets', query: '' };
+    const endpoint = mockEndpoint(params, 200, planetsMock.onePage);
+    return swapiService.get(params).then(obj => {
+      expect(obj.data).toHaveProperty('count');
+      expect(obj.data).toHaveProperty('results');
+      expect(obj.data).toHaveProperty('previous');
+      expect(obj.data.previous).toBe(null);
+      expect(obj.data.count).toBe(60);
+      expect(obj.data.next).not.toBe(null);
+      expect(obj.data.results.length).toBe(10);
+    });
+  });
+  test('Get invalid planets page returns 404', () => {
+    params = { route: 'page', query: '?page=1000000' };
+    const endpoint = mockEndpoint(params, 404, {});
+    return swapiService.get(params).then(obj => {
+      const { response } = obj;
+      expect(response.status).toBe(404);
+    });
+  });
+  test('Get valid last planets page returns planets, but no next page ref', () => {
+    params = { route: 'planets', query: '?page=6' };
+    const endpoint = mockEndpoint(params, 200, planetsMock.lastPage);
+    return swapiService.get(params).then(obj => {
+      expect(obj.data).toHaveProperty('count');
+      expect(obj.data).toHaveProperty('results');
+      expect(obj.data).toHaveProperty('previous');
+      expect(obj.data).toHaveProperty('next');
+      expect(obj.data.next).toBe(null);
+      expect(obj.data.previous).not.toBe(null);
+      expect(obj.data.count).toBe(60);
+      expect(obj.data.results.length).toBe(10);
     });
   });
 });
