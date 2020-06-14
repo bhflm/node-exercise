2733a3845d0fdff0282a58060f03eb760236346b
German Bonin <germanbonin@gmail.com>
2020-06-13 22:12:29 -0300

    Add: correct people controller tests

--- tests/controllers/people.test.js
+++ tests/controllers/people.test.js
@@ -5,43 +5,44 @@ const server = require('../../app');
 const peopleMock = require('../mocks/people');
 
 const extractNumberValue = value => {
+  // unknown is mapped as 0 for avoiding NaN and correct comparison
   if (value === 'unknown') return 0;
   return Number(value);
 };
 
-describe.only('People controller tests', () => {
+describe('People controller tests', () => {
   const defaultTimeout = 100000;
-  // test(
-  //   'status is 200 with no sortBy',
-  //   async done => {
-  //     const res = await request(server).get('/people');
-  //     expect(res.status).toBe(200);
-  //     expect(res.body).toHaveProperty('data');
-  //     expect(res.body).toHaveProperty('count');
-  //     expect(res.body.data.count).not.toBe(0);
-  //     return done();
-  //   },
-  //   defaultTimeout
-  // );
-  // test(
-  //   'status is 200 and sort by height is ok',
-  //   async done => {
-  //     const res = await request(server).get('/people?sortBy=height');
-  //     expect(res.status).toBe(200);
-  //     expect(res.body).toHaveProperty('data');
-  //     expect(res.body).toHaveProperty('count');
-  //     const { data } = res.body;
-  //     expect(data.count).not.toBe(0);
-  //     const firstPerson = data[0];
-  //     const middlePerson = data[Math.ceil(data.length / 2)]
-  //     const lastPerson = data[data.length - 1];
-  //     expect(Number(firstPerson.height) < Number(lastPerson.height)).toBe(true);
-  //     expect(Number(firstPerson.height) > Number(middlePerson.height)).toBe(false);
-  //     expect(Number(middlePerson.height) < Number(lastPerson.height)).toBe(true);
-  //     return done();
-  //   },
-  //   defaultTimeout
-  // );
+  test(
+    'status is 200 with no sortBy',
+    async done => {
+      const res = await request(server).get('/people');
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty('data');
+      expect(res.body).toHaveProperty('count');
+      expect(res.body.data.count).not.toBe(0);
+      return done();
+    },
+    defaultTimeout
+  );
+  test(
+    'status is 200 and sort by height is ok',
+    async done => {
+      const res = await request(server).get('/people?sortBy=height');
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty('data');
+      expect(res.body).toHaveProperty('count');
+      const { data } = res.body;
+      expect(data.count).not.toBe(0);
+      const firstPerson = extractNumberValue(data[0].height);
+      const middlePerson = extractNumberValue(data[Math.ceil(data.length / 2)].height);
+      const lastPerson = extractNumberValue(data[data.length - 1].height);
+      expect(firstPerson > lastPerson).toBe(false);
+      expect(firstPerson < middlePerson).toBe(true);
+      expect(middlePerson < lastPerson).toBe(true);
+      return done();
+    },
+    defaultTimeout
+  );
   test(
     'status is 200 and sort by mass',
     async done => {
@@ -52,9 +53,8 @@ describe.only('People controller tests', () => {
       const { data } = res.body;
       expect(data.count).not.toBe(0);
       const firstPerson = extractNumberValue(data[0].mass);
-      const middlePerson = extractNumberValue(data[Math.ceil(data.length / 2)].mass);=
+      const middlePerson = extractNumberValue(data[Math.ceil(data.length / 2)].mass);
       const lastPerson = extractNumberValue(data[data.length - 1].mass);
-      // Add the '|| 0' statement because the custom sort sends the unknowns to the beggining;
       expect(firstPerson > lastPerson).toBe(false);
       expect(firstPerson < middlePerson).toBe(true);
       expect(middlePerson < lastPerson).toBe(true);
@@ -62,23 +62,23 @@ describe.only('People controller tests', () => {
     },
     defaultTimeout
   );
-  // test(
-  //   'status is 200 and sort by name',
-  //   async done => {
-  //     const res = await request(server).get('/people?sortBy=name');
-  //     expect(res.status).toBe(200);
-  //     expect(res.body).toHaveProperty('data');
-  //     expect(res.body).toHaveProperty('count');
-  //     const { data } = res.body;
-  //     expect(data.count).not.toBe(0);
-  //     const firstPerson = data[0];
-  //     const middlePerson = data[Math.ceil(data.length / 2)]
-  //     const lastPerson = data[data.length - 1];
-  //     expect(firstPerson.name < lastPerson.name).toBe(true);
-  //     expect(firstPerson.height < middlePerson.name).toBe(true);
-  //     expect(middlePerson.name > lastPerson.name).toBe(false);
-  //     return done();
-  //   },
-  //   defaultTimeout
-  // );
+  test(
+    'status is 200 and sort by name',
+    async done => {
+      const res = await request(server).get('/people?sortBy=name');
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty('data');
+      expect(res.body).toHaveProperty('count');
+      const { data } = res.body;
+      expect(data.count).not.toBe(0);
+      const firstPerson = data[0].name;
+      const middlePerson = data[Math.ceil(data.length / 2)].name
+      const lastPerson = data[data.length - 1].name;
+      expect(firstPerson < lastPerson).toBe(true);
+      expect(firstPerson < middlePerson).toBe(true);
+      expect(middlePerson > lastPerson).toBe(false);
+      return done();
+    },
+    defaultTimeout
+  );
 });
